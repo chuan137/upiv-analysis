84545e766a7202c4831abef89a082bccbe0145c3
Chuan Miao <chuan137@gmail.com>
2016-02-25 12:16:03 +0100

    Update azimuthal.py, better radial_profile algorithm

--- snr/azimuthal.py
+++ snr/azimuthal.py
@@ -35,16 +35,18 @@ def local_peak(data, window=5):
 
     index = []
     contrast = []
+    truth = [ True for i in range(win_half) ] + [ False for i in range(win_half) ]
 
     for i in range(0, len(data)-win_half):
     #for i in range(len(data)-win_half, 0, -1):
         dd = data[i:i+window]
         if not np.isnan(dd).any():
             test = (dd[1:] - dd[:-1] > 0)
-            if len(test) == 4:
-                if (test == [True, True, False, False]).all():
+            if len(test) == window - 1:
+                if (test == truth).all():
                     index.append(i+win_half)
-                    contrast.append( float(max(dd)-min(dd)) / window )
+                    # contrast.append( float(max(dd)-min(dd)) / min(dd))
+                    contrast.append( float(max(dd)-min(dd)) / dd.mean())
     return index, contrast
 
 
@@ -58,7 +60,8 @@ def best_local_peak(data, window=5):
 def radial_profile_simple(image, center, r0=60):
     r0 = int(r0)
     maxr = r0 + 10
-    minr = max(min(r0/2, r0-10), 2)
+    minr = max(min(r0/2, r0-10), 3)
+    # print minr, r0, maxr
 
     cropped = crop_image_c(image, center, maxr)
     xx, yy = np.mgrid[-maxr:maxr+1, -maxr:maxr+1]
@@ -66,16 +69,19 @@ def radial_profile_simple(image, center, r0=60):
 
     prof = []
     for rr in range(minr, maxr):
-        s = cropped[np.logical_and(rad > rr,  rad <= rr+1)]
+        s = cropped[np.logical_and(rad >= rr,  rad <= rr+2)]     # ring width 2
         prof.append(s[s>0].mean())
     prof = np.array(prof)
 
     try:
-        r_peak, v_peak, _  = best_local_peak(prof)
-        r_peak += minr + 0.5
+        r_peak, v_peak, _  = best_local_peak(prof, window=3)
+        r_peak = r_peak + minr + 1
     except TypeError as e:
-        sys.stderr.write(e.message + '\n')
+        message = e.message
+        message += ", ring: %d %d %d" % (center[0], center[1], r0)
+        sys.stderr.write(message + '\n')
         r_peak, v_peak = 0, 0
-    return r_peak, v_peak
+    # return r_peak, v_peak
+    return prof, r_peak, v_peak
 
 
