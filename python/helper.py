c3054f026fbb957a862dbeb44538939e7d9ff5cf
Chuan Miao <chuan137@gmail.com>
2016-02-25 10:17:08 +0100

    Add stats function; Clean up

--- python/helper.py
+++ python/helper.py
@@ -1,16 +1,7 @@
-
-# coding: utf-8
-
-# In[ ]:
-
 import numpy as np
 import operator
 import re
 
-
-# ### manuplating files
-
-# In[ ]:
 
 def ext_filter(path, extension):
     """ return file-[] with extension, in sorted order"""
@@ -18,6 +9,7 @@ def ext_filter(path, extension):
     from os.path import isfile, join
     filtered_files = [join(path, f) for f in listdir(path) if isfile(join(path, f))]
     return sorted(filtered_files)
+
 
 def read_raw(filename, xsize=1024, ysize=1024):
     import struct
@@ -40,7 +32,6 @@ def reverse_enum(L):
     for index in reversed(range(len(L))):
         yield index, L[index]
 
-        
 def crop_image(img, corner, xr=50, yr=50):
     x0, y0 = corner
     return np.array(img[y0:y0+yr, x0:x0+xr])
@@ -54,119 +45,6 @@ def smooth_array(lst):
     res[0] = 0.75*lst[0] + 0.25*lst[1]
     res[-1] = 0.75*lst[-1] + 0.25*lst[-2]
     return np.array(res)
-        
-
-def azimu_hist(img, center_x, center_y):
-    hist_x = np.arange(0, 50, 0.5)
-    hist_n = np.zeros_like(hist_x)
-    hist = np.zeros_like(hist_x)
-    for i,j in coordinates(img.shape[0], img.shape[1]):
-        dist = np.sqrt((i-center_x)**2 + (j-center_y)**2)
-        hist[int(dist/0.5)] += img[i,j]
-        hist_n[int(dist/0.5)] += 1
-    return hist_x, hist/hist_n
-
-
-def local_peak(data, window=5):
-    """ Search local peak in data[].
-
-    A peak is a series of values, that first monotonically increases
-    to the middle point of the series, then monotonically decreases.
-    The length of the series is given by *window*, that should be odd.
-
-    @return (index[], contrast[])
-        index:    of each local peak
-        contrast: max subtract min of each peak series, normalized
-                  by window
-    """
-    if window != 2 * (window / 2) + 1:
-        warnings.warn("window (%d) is not an odd number. Increasing by 1" % window)
-        window += 1
-    win_half = window / 2
-    
-    index = []
-    contrast = []
-
-    for i in range(0, len(data)-win_half):
-    #for i in range(len(data)-win_half, 0, -1):
-        dd = data[i:i+window]
-        if not np.isnan(dd).any():
-            test = (dd[1:] - dd[:-1] > 0)
-            if len(test) == 4:
-                if (test == [True, True, False, False]).all():
-                    index.append(i+win_half)
-                    contrast.append( float(max(dd)-min(dd)) / window )
-    return index, contrast
-
-
-def best_local_peak(data, window=5):
-    peaks, contrasts = local_peak(data, window)
-    if len(peaks) > 0:
-        peak_val = np.array([data[i] for i in peaks])
-        return max(zip(peaks, peak_val, contrasts), key=operator.itemgetter(1))
-
-
-# In[ ]:
-
-def radial_profile(img, px=None, py=None, minr=5, maxr=50, bins=None):
-    """ Calculate azimuthally averaged radial profile
-    
-    px:     coordinate x of center. if None, take the center of image.
-    py:     coordinate y of center.
-    maxr:   max radius in profile. if (px, py) is very close to border, change 
-            maxr to the distance of (px,py) to bondary
-    bins:   bins for histogram. if a list, histogram bins. if a integer, bins
-            is number of bins. if None, histogram bins is arange(0, maxr, 0.5)
-    """
-    
-    if px is None:
-        centerx = int(img.shape[1]/2) - 1
-    if py is None:
-        py = img.shape[0]/2 - 1
-    
-    maxr_ = min(px, py, img.shape[1] - px, img.shape[0] - py)    
-    if maxr + 2 > maxr_:
-        maxr = maxr_ - 2
-        
-    if bins is None:
-        bins = np.arange(0, maxr, 0.5)
-    if type(bins) is int:
-        bins = np.arange(0, maxr, float(marx/bins))
-    
-    # crop original image from (px - maxr, py - maxr) with size
-    # (2*maxr + 1, 2*maxr + 1)
-    crop_ = crop_image(img, (px-maxr, py-maxr), 2*maxr+1, 2*maxr+1)
-    
-    # calculate profile at (maxr, maxr)
-    y, x = np.indices((crop_.shape))
-    r = np.sqrt((x - maxr)**2 + (y - maxr)**2)
-
-    hr = np.histogram(r.ravel(), bins=bins)
-    h = np.histogram(r.ravel(), bins=bins, weights=crop_.ravel())
-    
-    bins = (0.5 * (bins[1:] + bins[:-1]))
-    res = h[0] / hr[0]
-    res[bins<minr] = float('NaN')
-    return bins, res
-
-
-# In[1]:
-#
-# def read_rings(fpath):
-#     def ring_filter(r):
-#         return tuple(map(float, r[:-1].split()[:3]))
-#     
-#     rings = []
-#     
-#     with open(fpath, 'r') as df:
-#         for l in df.readlines():
-#             if l[0] == '#':
-#                 rings.append([])
-#             else:
-#                 rings[-1].append(l)
-#
-#     return [[ring_filter(item) for item in row] for row in rings]
-#
 
 def read_rings(ring_file, cols=(0,1,2)):
     '''
@@ -221,6 +99,8 @@ def loadtxt(fname, usecols=None):
     return np.array(data)
 
 
+def stats(arr):
+    return arr.size, arr.sum(), arr.mean(), arr.std(), arr.min(), arr.max()
+
 def print_stats(arr):
-    print 'size: {}, sum: {}, mean: {}, std: {}, min: {}, max: {}'.format(\
-        arr.size, arr.sum(), arr.mean(), arr.std(), arr.min(), arr.max())
+    print 'size: {}, sum: {}, mean: {}, std: {}, min: {}, max: {}'.format(stats(arr))
